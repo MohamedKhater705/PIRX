3889da41331e4637c32c4f5e019dca17f2f2a13c
unknown <mohamedkhater7005@gmail.com>
2026-07-30 14:17:09 +0300

    Add public JSON fallback for mobile network support

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,8 +3,19 @@ import type { Product } from "@/types";
 const BASE_URL = "https://fakestoreapi.com";
 
 export async function getAllProducts(): Promise<Product[]> {
-  const res = await fetch(`${BASE_URL}/products`);
-  if (!res.ok) throw new Error("Failed to fetch products");
-  const data = res.json();
-  return data;
+  try {
+    const res = await fetch(`${BASE_URL}/products`);
+    if (!res.ok)
+      throw new Error(
+        "Oops!, something went wrong while getting your products",
+      );
+    return await res.json();
+  } catch (error) {
+    console.warn(
+      "API blocked or offline. Switching to local fallback...",
+      error,
+    );
+    const fallbackRes = await fetch("/PIRX/products.json");
+    return await fallbackRes.json();
+  }
 }
