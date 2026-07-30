461d674862d13c0f7b191254a56bd4c102855ce7
unknown <mohamedkhater7005@gmail.com>
2026-07-30 14:31:12 +0300

    fixed  public JSON fallback for mobile network support

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,11 @@
 import type { Product } from "@/types";
 
-const BASE_URL = "https://fakestoreapi.com";
+// Renamed to API_URL so it doesn't get confused with Vite's import.meta.env.BASE_URL
+const API_URL = "https://fakestoreapi.com";
 
 export async function getAllProducts(): Promise<Product[]> {
   try {
-    const res = await fetch(`${BASE_URL}/products`);
+    const res = await fetch(`${API_URL}/products`);
     if (!res.ok)
       throw new Error(
         "Oops!, something went wrong while getting your products",
@@ -15,7 +16,16 @@ export async function getAllProducts(): Promise<Product[]> {
       "API blocked or offline. Switching to local fallback...",
       error,
     );
-    const fallbackRes = await fetch("/PIRX/products.json");
+
+    // import.meta.env.BASE_URL points to your site's root (/PIRX/)
+    const fallbackRes = await fetch(`${import.meta.env.BASE_URL}products.json`);
+
+    if (!fallbackRes.ok) {
+      throw new Error("Fallback JSON file could not be loaded!", {
+        cause: error,
+      });
+    }
+
     return await fallbackRes.json();
   }
 }
